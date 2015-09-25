3d69e67d2d86519343c787ee37f9d2a223f000e3
kathrynschlagel <G@be7185>
2015-09-25 17:57:16 -0500

    initial commit.

---
+++ scripts/js/_vti_cnf/dropdownmenu.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|Anna-PC\\Anna_2
+vti_modifiedby:SR|Anna-PC\\Anna_2
+vti_timelastmodified:TR|11 Sep 2012 03:48:01 -0000
+vti_timecreated:TR|07 Jul 2012 03:39:41 -0000
+vti_extenderversion:SR|12.0.0.6211
+vti_backlinkinfo:VX|contact.html gallery_one.html artist_statement.html gallery_two.html gallery.html Archives/maillette-front.dwt gallery_three.html Archives/maillette-others.dwt artist_resume.html index.html Archives/maillette-gallery.dwt
+vti_nexttolasttimemodified:TR|07 Jul 2012 03:45:31 -0000
+vti_cacheddtm:TX|11 Sep 2012 03:48:01 -0000
+vti_filesize:IR|824
---
+++ scripts/js/_vti_cnf/lightbox.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|11 Sep 2012 03:48:11 -0000
+vti_extenderversion:SR|12.0.0.6211
+vti_author:SR|Anna-PC\\Anna_2
+vti_modifiedby:SR|Anna-PC\\Anna_2
+vti_timecreated:TR|11 Sep 2012 03:33:56 -0000
+vti_backlinkinfo:VX|gallery_one.html gallery.html gallery_two.html gallery_three.html Archives/maillette-gallery.dwt
+vti_nexttolasttimemodified:TR|11 Sep 2012 03:46:58 -0000
+vti_cacheddtm:TX|11 Sep 2012 03:48:11 -0000
+vti_filesize:IR|11597
